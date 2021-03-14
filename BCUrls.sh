1e1fc82c02d61000e815e75febd0c294e60a4999
nullman0x <77861653+nullman0x@users.noreply.github.com>
2021-03-15 03:22:47 +0530

    Update BCUrls.sh

--- BCUrls.sh
+++ BCUrls.sh
@@ -12,25 +12,3 @@ do
 
 done
 
-paste Private.txt | while read if
-#gets URLS
-do
-#curl 'https://bugcrowd.com/'$if'' -H 'cookie: _crowdcontrol_session='$1''  --compressed | pup 'title' | grep -v '<' >>  PrivatePrograms.txt
-
-
-
-#curl 'https://bugcrowd.com/'$if'' -H 'cookie: _crowdcontrol_session='$1''  --compressed | pup 'div#user-guides__bounty-brief__targets-table' | pup 'code' | grep -v '<' >> PrivatePrograms.txt
-
-done
-paste Public.txt | while read of
-do
-
-
-
-#curl 'https://bugcrowd.com/'$of'' --compressed | pup 'h1[class="bc-panel__title"]' | grep -v '<' >> PublicPrograms.txt
-
-#curl 'https://bugcrowd.com/'$of'' --compressed | pup 'div#user-guides__bounty-brief__targets-table' | pup 'code' | grep -v '<'  >>  PublicPrograms.txt
-
-
-
-done
